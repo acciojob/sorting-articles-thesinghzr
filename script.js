70aec0f7133adb5e1e00c5eb55c549525772ce25
Suraj Singh <yousurajsingh@gmail.com>
2024-12-29 22:20:13 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,29 +1,31 @@
-//your code here
-let touristSpots = ['The Virupaksha Temple', 'Victoria Memorial', 'Tajmahal'];
+const bands = [
+  'The Plot in You',
+  'The Devil Wears Prada',
+  'Pierce the Veil',
+  'Norma Jean',
+  'The Bled',
+  'Say Anything',
+  'The Midway State',
+  'We Came as Romans',
+  'Counterparts',
+  'Oh, Sleeper',
+  'A Skylit Drive',
+  'Anywhere But Here',
+  'An Old Dog'
+];
 
-// const bands = ['The Plot in You', 'The Devil Wears Prada',
-//      'Pierce the Veil', 'Norma Jean', 'The Bled', 
-//      'Say Anything', 'The Midway State', 'We Came as Romans ',
-//       'Counterparts', 'Oh, Sleeper', 'A Skylit Drive', 
-//       'Anywhere But Here', 'An Old Dog'];
-
-let sortedSpots = touristSpots.sort((a,b)=>{
-    return strip(a).localeCompare(strip(b))
-});
-
-function strip(name){
-    return name.replace(/^(a |an |the )/i,'').trim();                              
+// Function to strip the articles from the names
+function stripArticle(name) {
+  return name.replace(/^(a |an |the )/i, '').trim();
 }
 
-const unorderedList = document.querySelector('ul');
-unorderedList.setAttribute("id","bands");
-unorderedList.setAttribute("id","band");
+// Sort the bands array ignoring articles
+const sortedBands = bands.sort((a, b) => (stripArticle(a) > stripArticle(b) ? 1 : -1));
 
-
-sortedSpots.forEach((spot)=>{
-    const list = document.createElement('li');
-    list.textContent = spot;
-    unorderedList.appendChild(list);
-})
-
-console.log(sortedSpots);
+// Add sorted items to the UI
+const bandsList = document.getElementById('bands');
+sortedBands.forEach(band => {
+  const li = document.createElement('li');
+  li.textContent = band;
+  bandsList.appendChild(li);
+});
